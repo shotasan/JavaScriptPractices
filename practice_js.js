67dd5187affeed91e0b4cdaf933aca691025a7aa
shotasan <wondermoai@gmail.com>
2019-06-03 22:13:37 +0900

    No.1

---
+++ practice_js.js
@@ -0,0 +1,15 @@
+// No.1
+// Very simple, given a number, find its opposite.
+// 1: -1
+// 14: -14
+//   - 34: 34
+
+// My_answer
+function opposite(number) {
+  return (-number);
+}
+
+// Best_answer
+function opposite(number) {
+  return (-number);
+}
