ae5e7e8dc6d9fe7b7951095dba0929cb04e9ec0b
shotasan <wondermoai@gmail.com>
2019-06-06 15:24:01 +0900

    No.2

--- practice_js.js
+++ practice_js.js
@@ -13,3 +13,18 @@ function opposite(number) {
 function opposite(number) {
   return (-number);
 }
+
+// No.2
+// Write a function called repeatStr which repeats the given string string exactly n times.
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+// My_answer
+function repeatStr(n, s) {
+  return s.repeat(n);
+}
+
+// Best_answer
+function repeatStr(n, s) {
+  return s.repeat(n);
+}
