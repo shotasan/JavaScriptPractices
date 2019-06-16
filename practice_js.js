6da059c3cddbe3d63f71bf367d1b2e870e8468cc
shotasan <wondermoai@gmail.com>
2019-06-16 16:54:03 +0900

    No.4

--- practice_js.js
+++ practice_js.js
@@ -45,3 +45,28 @@ function removeChar(str) {
 function removeChar(str) {
   return str.slice(1).slice(0, -1)
 };
+
+
+// No.4
+// In this simple assignment you are given a number and have to make it negative.
+// But maybe the number is already negative ?
+// Example :
+// makeNegative(1); // return -1
+// makeNegative(-5); // return -5
+// makeNegative(0); // return 0
+// makeNegative(0.12); // return -0.12
+
+// My_answer
+function makeNegative(num) {
+  if (num > 0) {
+    return -num;
+  }
+  else {
+    return num;
+  }
+}
+
+// Best_answer
+function makeNegative(num) {
+  return -Math.abs(num);
+}
