758bf70fae4d89afca7772108417a064c2343591
shotasan <wondermoai@gmail.com>
2019-06-19 23:42:59 +0900

    No.6

--- practice_js.js
+++ practice_js.js
@@ -91,3 +91,21 @@ class SmallestIntegerFinder {
     return Math.min(...args)
   }
 }
+
+
+// No.6
+// Description:
+// Complete the solution so that it reverses all of the words within the string passed in.
+// Example:
+// reverseWords("The greatest victory is that which requires no battle")
+// should return "battle no requires which that is victory greatest The"
+
+// My_answer
+function reverseWords(str) {
+  return str.split(' ').reverse().join(' ');
+}
+
+// Best_answer
+function reverseWords(str) {
+  return str.split(' ').reverse().join(' ');
+}
