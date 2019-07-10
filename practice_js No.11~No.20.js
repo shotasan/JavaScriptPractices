dfea383586e1b9d05698b7e38cfb5bae36a5838f
shotasan <wondermoai@gmail.com>
2019-07-10 16:31:32 +0900

    No.14

--- practice_js No.11~No.20.js
+++ practice_js No.11~No.20.js
@@ -105,3 +105,28 @@ function numberToString(num) {
 function numberToString(num) {
   return num.toString();
 }
+
+
+// No.14
+// Jenny has written a function that returns a greeting for a user.
+// However, she's in love with Johnny, and would like to greet him slightly different.
+// She added a special case to her function, but she made a mistake.
+// Can you help her ?
+// function greet(name) {
+//   return "Hello, " + name + "!";
+//   if (name === "Johnny")
+//     return "Hello, my love!";
+// }
+
+// My_answer
+function greet(name) {
+  if (name === "Johnny")
+    return "Hello, my love!";
+  else
+    return "Hello, " + name + "!";
+}
+
+// Best_answer
+function greet(name) {
+  return "Hello, " + (name == "Johnny" ? "my love" : name) + "!";
+}
