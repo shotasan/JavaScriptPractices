b3d95643e3d2787ad97c244b448ce8e88ab7a16d
shotasan <wondermoai@gmail.com>
2019-07-12 19:21:22 +0900

    No.16

--- practice_js No.11~No.20.js
+++ practice_js No.11~No.20.js
@@ -152,3 +152,21 @@ function abbrevName(name) {
   var nameArray = name.split(" ");
   return (nameArray[0][0] + "." + nameArray[1][0]).toUpperCase();
 }
+
+
+// No.16
+// Can you find the needle in the haystack ?
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+// After your function finds the needle it should return a message(as a string) that says:
+// "found the needle at position " plus the index it found the needle, so:
+
+// My_answer
+function findNeedle(haystack) {
+  let position = haystack.indexOf('needle')
+  return `found the needle at position ${position}`
+}
+
+// Best_answer
+function findNeedle(haystack) {
+  return "found the needle at position " + haystack.indexOf("needle");
+}
