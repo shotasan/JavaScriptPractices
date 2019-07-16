a3c428e2069bc17c0fa5dbe95a2434fd07d3a0c7
shotasan <wondermoai@gmail.com>
2019-07-16 22:03:59 +0900

    No.19

--- practice_js No.11~No.20.js
+++ practice_js No.11~No.20.js
@@ -211,3 +211,22 @@ function positiveSum(arr) {
 function positiveSum(arr) {
   return arr.reduce((a, b) => a + (b > 0 ? b : 0), 0);
 }
+
+
+// No.19
+// Create a function isDivisible(n, x, y) that checks if a number n is divisible by two numbers x AND y.All inputs are positive, non - zero digits.
+// Example:
+// isDivisible(3, 1, 3)-- > true because 3 is divisible by 1 and 3
+// isDivisible(12, 2, 6)-- > true because 12 is divisible by 2 and 6
+// isDivisible(100, 5, 3)-- > false because 100 is not divisible by 3
+// isDivisible(12, 7, 5)-- > false because 12 is neither divisible by 7 nor 5
+
+// My_answer
+function isDivisible(n, x, y) {
+  return n % x == 0 && n % y == 0 ? true : false
+}
+
+// Best_answer
+function isDivisible(n, x, y) {
+  return n % x === 0 && n % y === 0
+}
