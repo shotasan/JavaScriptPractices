b23aaa77b4ebeac4b991c5ca0ee4b3791462d006
shotasan <wondermoai@gmail.com>
2019-07-19 21:27:27 +0900

    No.22

--- practice_js No.21~No.30.js
+++ practice_js No.21~No.30.js
@@ -13,3 +13,17 @@ function squareSum(numbers) {
     return (n * n) + sum;
   }, 0)
 }
+
+// No.22
+// Complete the solution so that it reverses the string value passed into it.
+// solution('world'); // returns 'dlrow'
+
+// My_answer
+function solution(str) {
+  return str.split('').reverse().join('');
+}
+
+// Best_answer
+function solution(str) {
+  return str.split('').reverse().join('');
+}
