41fc7fa65b60587b3fe4f6d126844c003dd59846
shotasan <wondermoai@gmail.com>
2019-07-20 13:53:45 +0900

    No.23

--- practice_js No.21~No.30.js
+++ practice_js No.21~No.30.js
@@ -27,3 +27,29 @@ function solution(str) {
 function solution(str) {
   return str.split('').reverse().join('');
 }
+
+
+// No.23
+// Your task is to make two functions,
+// max and min(maximum and minimum in PHP and Python) that take a(n) array / vector of integers list as input and outputs,
+// respectively, the largest and lowest number in that array / vector.
+// #Examples
+// max([4, 6, 2, 1, 9, 63, -134, 566]) returns 566
+// min([-52, 56, 30, 29, -54, 0, -110]) returns - 110
+// max([5]) returns 5
+// min([42, 54, 65, 87, 0]) returns 0
+// #Notes
+// You may consider that there will not be any empty arrays / vectors.
+
+// My_answer
+var min = function (list) {
+  return list.reduce((a, b) => Math.min(a, b))
+}
+
+var max = function (list) {
+  return list.reduce((a, b) => Math.max(a, b));
+}
+
+// Best_answer
+const min = (list) => Math.min(...list);
+const max = (list) => Math.max(...list);
