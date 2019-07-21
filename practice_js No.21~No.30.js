fc9b02901bd987719f76d8b8da0f1e99b2134ca8
shotasan <wondermoai@gmail.com>
2019-07-21 18:33:13 +0900

    No.24

--- practice_js No.21~No.30.js
+++ practice_js No.21~No.30.js
@@ -53,3 +53,56 @@ var max = function (list) {
 // Best_answer
 const min = (list) => Math.min(...list);
 const max = (list) => Math.max(...list);
+
+
+// No.24
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+// Examples:
+// rps('scissors', 'paper') // Player 1 won!
+// rps('scissors', 'rock') // Player 2 won!
+// rps('paper', 'paper') // Draw!
+
+// My_answer
+const rps = (p1, p2) => {
+  switch (p1) {
+    case 'rock':
+      switch (p2) {
+        case 'rock':
+          return 'Draw!';
+        case 'scissors':
+          return 'Player 1 won!';
+        case 'paper':
+          return 'Player 2 won!';
+      }
+    case 'scissors':
+      switch (p2) {
+        case 'rock':
+          return 'Player 2 won!';
+        case 'scissors':
+          return 'Draw!';
+        case 'paper':
+          return 'Player 1 won!';
+      }
+    case 'paper':
+      switch (p2) {
+        case 'rock':
+          return 'Player 1 won!';
+        case 'scissors':
+          return 'Player 2 won!';
+        case 'paper':
+          return 'Draw';
+      }
+  }
+};
+
+// Best_answer
+const rps = (p1, p2) => {
+  if (p1 === p2) return "Draw!";
+  var rules = { rock: "scissors", paper: "rock", scissors: "paper" };
+  if (p2 === rules[p1]) {
+    return "Player 1 won!";
+  }
+  else {
+    return "Player 2 won!";
+  }
+};
