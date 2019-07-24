487f97bf24b992e9e6dea1f86098b52ba0557dff
shotasan <wondermoai@gmail.com>
2019-07-24 15:21:53 +0900

    No.33

--- practive_js No.31~No.40.js
+++ practive_js No.31~No.40.js
@@ -32,3 +32,26 @@ function maps(x) {
 function maps(x) {
   return x.map(n => n * 2);
 }
+
+
+// No.33
+// Our football team finished the championship.The result of each match look like "x:y".
+// Results of all matches are recorded in the collection.
+// For example: ["3:1", "2:2", "0:1", ...]
+// Write a function that takes such collection and counts the points of our team in the championship.
+// Rules for counting points for each match:
+// if x > y - 3 points
+// if x < y - 0 point
+// if x = y - 1 point
+// Notes:
+// there are 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+// Best_answer
+
+// reduceの引数currentにスコア(3:1)が入るのでインデックスを使用して数値を比較する
+// outputに結果の合計値が入る
+const points = games => games.reduce((output, current) => {
+  return output += current[0] > current[2] ? 3 : current[0] === current[2] ? 1 : 0;
+}, 0)
