98659c7edad671ec1ed2a581322d18bc793e953b
shotasan <wondermoai@gmail.com>
2019-08-02 15:23:29 +0900

    No.42

--- practice_js No.41~No.50.js
+++ practice_js No.41~No.50.js
@@ -12,3 +12,29 @@ function findShort(s) {
 function findShort(s) {
   return Math.min.apply(null, s.split(' ').map(w => w.length));
 }
+
+
+// No.42
+// Check to see if a string has the same amount of 'x's and 'o's.
+// The method must return a boolean and be case insensitive.The string can contain any char.
+// Examples input / output:
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+// My_answer
+function XO(str) {
+  let chars = str.toLowerCase().split('');
+  let countOfX = chars.filter(char => char === 'x').length;
+  let countOfO = chars.filter(char => char === 'o').length;
+  return countOfX === countOfO ? true : false;
+}
+
+// Best_answer
+function XO(str) {
+  let x = str.match(/x/gi);
+  let o = str.match(/o/gi);
+  return (x && x.length) === (o && o.length);
+}
