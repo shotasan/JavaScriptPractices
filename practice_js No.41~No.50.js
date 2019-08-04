f64fab8576501e0b33d47cc591f9a85715b568ed
shotasan <wondermoai@gmail.com>
2019-08-04 16:07:51 +0900

    No.45

--- practice_js No.41~No.50.js
+++ practice_js No.41~No.50.js
@@ -73,3 +73,20 @@ function disemvowel(str) {
 function disemvowel(str) {
   return str.replace(/[aeiou]/gi, '');
 }
+
+
+// No.45
+// Given an array of digital numbers, return a new array of length number containing the last even numbers from the original array(in the same order).
+// The original array will be not empty and will contain at least "number" even numbers.
+// For example:
+// ([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
+// ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
+// ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
+
+// My_answer
+function evenNumbers(array, number) {
+  return array.filter(i => i % 2 === 0).slice(-number);
+}
+
+// Best_answer
+const evenNumbers = (array, number) => array.filter(item => item % 2 === 0).slice(-number);
