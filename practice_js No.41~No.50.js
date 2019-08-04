8fca59bfb02ebbbca2a69b5dcf37249393971b98
shotasan <wondermoai@gmail.com>
2019-08-04 15:59:07 +0900

    No.44

--- practice_js No.41~No.50.js
+++ practice_js No.41~No.50.js
@@ -55,3 +55,21 @@ function squareDigits(num) {
   return Number(('' + num).split('').map(function (val) { return val * val; }).join(''));
 
 }
+
+
+// No.44
+// Trolls are attacking your comment section!
+// A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+// Note: for this kata y isn't considered a vowel.
+
+// My_answer
+function disemvowel(str) {
+  return str.replace(/[aeiou]/gi, '');
+}
+
+// Best_answer
+function disemvowel(str) {
+  return str.replace(/[aeiou]/gi, '');
+}
