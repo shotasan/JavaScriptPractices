283a7e2e0c8efe76abd6346dee5acbf7f791d06c
shotasan <wondermoai@gmail.com>
2019-08-06 23:26:46 +0900

    No.47

--- practice_js No.41~No.50.js
+++ practice_js No.41~No.50.js
@@ -120,3 +120,23 @@ function isSquare(n) {
 function isSquare(n) {
   return Math.sqrt(n) % 1 === 0;
 }
+
+
+// No.47
+// Take 2 strings s1 and s2 including only letters from ato z.Return a new sorted string, the longest possible, containing distinct letters,
+// each taken only once - coming from s1 or s2.
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+// My_answer
+function longest(s1, s2) {
+  let letters = (s1 + s2).split('').sort();
+  return [...new Set(letters)].join('');
+}
+
+// Best_answer
+const longest = (s1, s2) => [...new Set(s1 + s2)].sort().join('')
