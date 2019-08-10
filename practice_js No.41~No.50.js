e7f48073686f2e50c7b3ed3d3bc9f4c06c9b524e
shotasan <wondermoai@gmail.com>
2019-08-10 12:14:40 +0900

    No.50

--- practice_js No.41~No.50.js
+++ practice_js No.41~No.50.js
@@ -179,3 +179,21 @@ function sumTwoSmallestNumbers(numbers) {
   numbers = numbers.sort(function (a, b) { return a - b; });
   return numbers[0] + numbers[1];
 };
+
+
+// No.50
+// In this kata you will create a function that takes a list of non - negative integers and strings and returns a new list with the strings filtered out.
+// Example
+// filter_list([1, 2, 'a', 'b']) == [1, 2]
+// filter_list([1, 'a', 'b', 0, 15]) == [1, 0, 15]
+// filter_list([1, 2, 'aasf', '1', '123', 123]) == [1, 2, 123]
+
+// My_answer
+function filter_list(l) {
+  return l.filter(e => Number.isInteger(e));
+}
+
+// Best_answer
+function filter_list(l) {
+  return l.filter(function (v) { return typeof v == 'number' })
+}
