360eb9ea05ed778ca97a20d85eddfaca170119a9
shotasan <wondermoai@gmail.com>
2019-08-12 22:51:33 +0900

    No.53

--- practice_js No.51~No.60.js
+++ practice_js No.51~No.60.js
@@ -36,3 +36,30 @@ const binaryArrayToNumber = arr => parseInt(arr.join(''), 2);
 
 // Best_answer
 const binaryArrayToNumber = arr => parseInt(arr.join(''), 2);
+
+// No.53
+// Create a function named divisors / Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself),
+// from smallest to largest.
+// If the number is prime return the string '(integer) is prime'.
+// Example:
+// divisors(12); // should return [2,3,4,6]
+// divisors(25); // should return [5]
+// divisors(13); // should return "13 is prime"
+
+// My_answer
+function divisors(integer) {
+  let results = [];
+  for (let i = 2; i < integer; i++) {
+    if (integer % i === 0) {
+      results.push(i);
+    }
+  }
+  return results.length ? results : `${integer} is prime`;
+};
+
+// Best_asnwer
+function divisors(integer) {
+  var res = []
+  for (var i = 2; i <= Math.floor(integer / 2); ++i) if (integer % i == 0) res.push(i);
+  return res.length ? res : integer + ' is prime'
+};
