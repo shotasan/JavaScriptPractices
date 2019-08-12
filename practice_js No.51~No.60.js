4daed42c0d075df06759b7b292f6fd83a3481368
shotasan <wondermoai@gmail.com>
2019-08-12 22:35:57 +0900

    No.52

--- practice_js No.51~No.60.js
+++ practice_js No.51~No.60.js
@@ -15,3 +15,24 @@ function validatePIN(pin) {
 function validatePIN(pin) {
   return /^(\d{4}|\d{6})$/.test(pin)
 }
+
+
+// No.52
+// Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+// Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+// Examples:
+// Testing: [0, 0, 0, 1] ==> 1
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 0, 1] ==> 5
+// Testing: [1, 0, 0, 1] ==> 9
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 1, 0] ==> 6
+// Testing: [1, 1, 1, 1] ==> 15
+// Testing: [1, 0, 1, 1] ==> 11
+// However, the arrays can have varying lengths, not just limited to 4.
+
+// My_answer
+const binaryArrayToNumber = arr => parseInt(arr.join(''), 2);
+
+// Best_answer
+const binaryArrayToNumber = arr => parseInt(arr.join(''), 2);
