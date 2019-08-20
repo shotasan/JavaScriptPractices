ad27a81a78bf17474c5d83e5655ab93439557d49
shotasan <wondermoai@gmail.com>
2019-08-20 16:53:16 +0900

    No.59

--- practice_js No.51~No.60.js
+++ practice_js No.51~No.60.js
@@ -213,3 +213,23 @@ function declareWinner(fighter1, fighter2, firstAttacker) {
     return firstAttacker;
   }
 }
+
+
+// No.59
+// Complete the function that accepts a string parameter, and reverses each word in the string.
+// All spaces in the string should be retained.
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces" ==> "elbuod  secaps"
+
+// My_answer
+function reverseWords(str) {
+  return str.split(' ').map(word => [...word].reverse().join('')).join(' ');
+}
+
+// Best_answer
+function reverseWords(str) {
+  return str.split(' ').map(function (word) {
+    return word.split('').reverse().join('');
+  }).join(' ');
+}
