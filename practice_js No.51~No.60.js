8d532a1cd80869872d643c59d772117f42414003
shotasan <wondermoai@gmail.com>
2019-08-21 12:21:41 +0900

    No.60

--- practice_js No.51~No.60.js
+++ practice_js No.51~No.60.js
@@ -233,3 +233,23 @@ function reverseWords(str) {
     return word.split('').reverse().join('');
   }).join(' ');
 }
+
+
+// No.60
+// Given an array of numbers(a list in groovy), determine whether the sum of all of the numbers is odd or even.
+// Give your answer in string format as 'odd' or 'even'.
+// If the input array is empty consider it as: [0](array with a zero).
+// Example:
+// oddOrEven([0]) returns "even"
+// oddOrEven([2, 5, 34, 6]) returns "odd"
+// oddOrEven([0, -1, -5]) returns "even"
+
+// My_answer
+function oddOrEven(array) {
+  return array.length === 0 || array.reduce((a, b) => a + b) % 2 === 0 ? "even" : "odd"
+}
+
+// Best_answer
+function oddOrEven(arr) {
+  return arr.reduce((a, b) => a + b, 0) % 2 ? 'odd' : 'even';
+}
