98017a6e5e4a0d7eaa0af89f132a588ca4fdd1c2
shotasan <wondermoai@gmail.com>
2019-08-22 21:13:24 +0900

    No.62

--- practice_js No.61~No.70.js
+++ practice_js No.61~No.70.js
@@ -20,3 +20,21 @@ function minMax(arr) {
 function minMax(arr) {
   return [Math.min(...arr), Math.max(...arr)];
 }
+
+
+// No.62
+// Your task is to split the chocolate bar of given dimension n x m into small squares.
+// Each square is of size 1x1 and unbreakable.Implement a function that will return minimum number of breaks needed.
+// For example if you are given a chocolate bar of size 2 x 1 you can split it to single squares in just one break, but for size 3 x 1 you must do two breaks.
+// If input data is invalid you should return 0(as in no breaks are needed if we do not have any chocolate to split).
+// Input will always be a non - negative integer.
+
+// My_answer
+function breakChocolate(n, m) {
+  return (n * m) > 0 ? (n * m) - 1 : 0;
+}
+
+// Best_answer
+var breakChocolate = function (n, m) {
+  return (n * m === 0) ? 0 : n * m - 1;
+};
