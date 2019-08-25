62b6aaab9a3bc94bf16b51b2b6ab277acfafad85
shotasan <wondermoai@gmail.com>
2019-08-25 23:01:54 +0900

    No.64

--- practice_js No.61~No.70.js
+++ practice_js No.61~No.70.js
@@ -55,3 +55,35 @@ function solution(str, ending) {
 function solution(str, ending) {
   return new RegExp(ending + "$", "i").test(str);
 }
+
+
+// No.64
+// Story
+// Your online store likes to give out coupons for special occasions.
+// Some customers try to cheat the system by entering invalid codes or using expired coupons.
+// Task
+// Your mission:
+// Write a function called which verifies that a coupon code is valid, the coupon is not expired.
+// A coupon is no more valid on the day AFTER the expiration date.
+// All dates will be passed as strings in this format: "MONTH DATE, YEAR".
+// Examples:
+// checkCoupon("123", "123", "July 9, 2015", "July 9, 2015") === true
+// checkCoupon("123", "123", "July 9, 2015", "July 2, 2015") === false
+
+// My_answer
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
+  return checkCode(enteredCode, correctCode) && checkDate(currentDate, expirationDate);
+}
+
+function checkCode(enteredCode, correctCode) {
+  return enteredCode === correctCode;
+}
+
+function checkDate(currentDate, expirationDate) {
+  return Date.parse(currentDate) <= Date.parse(expirationDate)
+}
+
+// Best_answer
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
+  return enteredCode === correctCode && Date.parse(expirationDate) >= Date.parse(currentDate)
+}
