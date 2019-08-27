ddb74ab3eb0c91e4c5080f2b534e319cc74fb0d3
shotasan <wondermoai@gmail.com>
2019-08-27 10:28:29 +0900

    No.66

--- practice_js No.61~No.70.js
+++ practice_js No.61~No.70.js
@@ -107,3 +107,43 @@ function sortByLength(array) {
 function sortByLength(array) {
   return array.sort((a, b) => a.length - b.length);
 };
+
+
+// No.66
+// Let us begin with an example:
+// Take a number: 56789. Rotate left, you get 67895.
+// Keep the first digit in place and rotate left the other digits: 68957.
+// Keep the first two digits in place and rotate the other ones: 68579.
+// Keep the first three digits and rotate left the rest: 68597.
+// Now it is over since keeping the first four it remains only one digit which rotated is itself.
+// You have the following sequence of numbers:
+// 56789 -> 67895 -> 68957 -> 68579 -> 68597
+// and you must return the greatest: 68957.
+// Task
+// Write function max_rot(n) which given a positive integer n returns the maximum number you got doing rotations similar to the above example.
+// So max_rot(or maxRot or ...depending on the language) is such as:
+// max_rot(56789) should return 68957
+// max_rot(38458215) should return 85821534
+
+// My_answer
+function maxRot(n) {
+  numbers = [...n.toString()];
+  let result = [n];
+  for (let i = 0; i < numbers.length; i++) {
+    numbers.push(numbers[i]);
+    numbers.splice(i, 1);
+    result.push(numbers.join(""));
+  }
+  return Math.max(...result);
+}
+
+// Best_answer
+function maxRot(n) {
+  var str = n.toString();
+  var arr = [str];
+  for (var i = 0; i <= str.length - 1; i++) {
+    str = str.slice(0, i) + str.slice(i + 1) + str[i];
+    arr.push(str.split().join());
+  }
+  return Math.max.apply(null, arr);
+}
