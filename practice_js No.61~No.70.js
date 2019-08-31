9e779a7b40f2a9c02cf04a9a6886398a9f0cd96b
shotasan <wondermoai@gmail.com>
2019-08-31 23:45:14 +0900

    No.69

--- practice_js No.61~No.70.js
+++ practice_js No.61~No.70.js
@@ -204,3 +204,17 @@ function toCamelCase(str) {
 function toCamelCase(str) {
   return str.replace(/[-_](.)/g, (_, c) => c.toUpperCase());
 }
+
+
+// No.69
+// Given a number n, return the number of positive odd numbers below n, EASY!
+// oddCount(7) //=> 3, i.e [1, 3, 5]
+// oddCount(15) //=> 7, i.e [1, 3, 5, 7, 9, 11, 13]
+
+// My_answer
+function oddCount(n) {
+  return Math.floor(n / 2);
+}
+
+// Best_answer
+const oddCount = n => Math.floor(n / 2);
