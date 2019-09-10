0d3e5fd74242c3d1dafbdd1fecf42810ed8515a6
shotasan <wondermoai@gmail.com>
2019-09-10 09:24:20 +0900

    No.75

--- practice_js No.71~No.80.js
+++ practice_js No.71~No.80.js
@@ -86,3 +86,22 @@ function noOdds(values) {
 
   return values.filter(isEven);
 }
+
+
+// No.75
+// Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+// For example:
+// sumDigits(10);  // Returns 1
+// sumDigits(99);  // Returns 18
+// sumDigits(-32); // Returns 5
+// Let's assume that all numbers in the input will be integer values.
+
+// My_answer
+function sumDigits(number) {
+  return [...Math.abs(number).toString()].reduce((a, b) => +a + +b, 0);
+}
+
+// Best_answer
+function sumDigits(number) {
+  return Math.abs(number).toString().split('').reduce(function (a, b) { return +a + +b }, 0);
+}
