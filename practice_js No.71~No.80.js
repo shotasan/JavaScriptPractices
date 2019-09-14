ccc4d32eba3d444d4de07e33f3869eb6ad33d253
shotasan <wondermoai@gmail.com>
2019-09-14 23:15:10 +0900

    No.78

--- practice_js No.71~No.80.js
+++ practice_js No.71~No.80.js
@@ -163,3 +163,29 @@ function solution(pairs) {
     .map(function (k) { return k + ' = ' + pairs[k] })
     .join(',');
 }
+
+
+// No.78
+// In mathematics, the factorial of a non - negative integer n, denoted by n! , is the product of all positive integers less than or equal to n.
+// For example: 5! = 5 * 4 * 3 * 2 * 1 = 120. By convention the value of 0! is 1.
+// Write a function to calculate factorial for a given input.
+// If input is below 0 or above 12 throw an exception of type ArgumentOutOfRangeException (C#) or IllegalArgumentException(Java) or RangeException(PHP) or throw a RangeError(JavaScript).
+// More details about factorial can be found here: http://en.wikipedia.org/wiki/Factorial
+
+// My_answer
+function factorial(n) {
+  if (n < 0 || n > 12) return "RangeError";
+  if (n === 0) return 1;
+  result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+// Best_answer
+function factorial(n) {
+  if (n < 0 || n > 12)
+    throw new RangeError();
+  return n <= 1 ? 1 : n * factorial(n - 1);
+}
