9e41a2adbc729a0f3163349157ba1fa8158f39e6
shotasan <wondermoai@gmail.com>
2019-09-17 10:10:53 +0900

    No.82

--- practice_js No.81~No.90.js
+++ practice_js No.81~No.90.js
@@ -44,3 +44,31 @@ function wave(str) {
 
   return result;
 }
+
+
+// No.82
+// Find the number of divisors of a positive integer n.
+// Random tests go up to n = 500000.
+// Examples
+// divisors 4 = 3 -- 1, 2, 4
+// divisors 5 = 2 -- 1, 5
+// divisors 12 = 6 -- 1, 2, 3, 4, 6, 12
+// divisors 30 = 8 -- 1, 2, 3, 5, 6, 10, 15, 30
+
+// My_answer
+function getDivisorsCnt(n) {
+  let count = 0
+  for (let i = 1; i < n + 1; i++) {
+    if (n % i == 0) count += 1;
+  }
+  return count;
+}
+
+// Best_answer
+function getDivisorsCnt(n) {
+  for (var d = 0, i = n; i > 0; i--) {
+    if (n % i == 0) d++;
+  }
+
+  return d;
+}
