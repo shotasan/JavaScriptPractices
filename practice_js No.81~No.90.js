4c19ee8cd0a3b79e163307dea09ae84ceb3a73ae
shotasan <wondermoai@gmail.com>
2019-09-20 10:01:39 +0900

    No.84

--- practice_js No.81~No.90.js
+++ practice_js No.81~No.90.js
@@ -100,3 +100,34 @@ String.prototype.toJadenCase = function () {
     return word.charAt(0).toUpperCase() + word.slice(1);
   }).join(" ");
 }
+
+
+// No.84
+// You are going to be given a word.Your job is to return the middle character of the word.
+// If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+// #Examples:
+// Kata.getMiddle("test") should return "es"
+// Kata.getMiddle("testing") should return "t"
+// Kata.getMiddle("middle") should return "dd"
+// Kata.getMiddle("A") should return "A"
+// #Input
+// A word(string) of length 0 < str < 1000(In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases).
+// You do not need to test for this.This is only here to tell you that you do not need to worry about your solution timing out.
+// #Output
+// The middle character(s) of the word represented as a string.
+
+// My_answer
+function getMiddle(s) {
+  let leng = [...s].length / 2;
+
+  if (s.length % 2 == 0) {
+    return [...s].slice(leng - 1, leng + 1).join('');
+  } else {
+    return [...s].slice(leng, leng + 1).join('');
+  }
+}
+
+// Best_answer
+function getMiddle(s) {
+  return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
+}
