b4bc011aff7d521f55ebe0f7aa01f3eddd48704c
shotasan <wondermoai@gmail.com>
2019-09-23 12:57:12 +0900

    No.86

--- practice_js No.81~No.90.js
+++ practice_js No.81~No.90.js
@@ -150,3 +150,33 @@ function removeUrlAnchor(url) {
 function removeUrlAnchor(url) {
   return url.split('#')[0];
 }
+
+
+// No.86
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+// The sum of these multiples is 23.
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+
+// My_answer
+function solution(number) {
+  let multiples = [];
+  for (let i = 1; i < number; ++i) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      multiples.push(i);
+    }
+  }
+  return multiples.reduce((a, b) => a + b, 0);
+}
+
+// Best_answer
+function solution(number) {
+  var sum = 0;
+
+  for (var i = 1; i < number; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum += i
+    }
+  }
+  return sum;
+}
