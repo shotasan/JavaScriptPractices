64b37cdeb14cd535da496c0a7d5d9d3ac991f2f6
shotasan <wondermoai@gmail.com>
2019-09-26 08:12:04 +0900

    N0.89

--- practice_js No.81~No.90.js
+++ practice_js No.81~No.90.js
@@ -285,3 +285,45 @@ Array.prototype.average = function () { return this.sum() / this.length; }
 Array.prototype.sum = function () { return this.reduce(function (a, b) { return a + b; }, 0); }
 Array.prototype.even = function () { return this.filter(function (item) { return 0 == item % 2; }); }
 Array.prototype.odd = function () { return this.filter(function (item) { return 0 != item % 2; }); }
+
+
+// No.89
+// Given two arrays a and b write a function comp(a, b) (compSame(a, b) in Clojure) that checks whether the two arrays have the "same" elements, with the same multiplicities.
+// "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+// Examples
+// Valid arrays
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+// comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on.
+// It gets obvious if we write b's elements in terms of squares:
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19]
+// Invalid arrays
+// If we change the first number to something else, comp may not return true anymore:
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+// comp(a, b) returns false because in b 132 is not the square of any number of a.
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [121, 14641, 20736, 36100, 25921, 361, 20736, 361]
+// comp(a, b) returns false because in b 36100 is not the square of any number of a.
+// Remarks
+// a or b might be[](all languages except R, Shell).a or b might be nil or null or None or nothing(except in Haskell, Elixir, C++, Rust, R, Shell, PureScript).
+// If a or b are nil(or null or None), the problem doesn't make sense so return false.
+// If a or b are empty then the result is self - evident.
+// a or b are empty or not empty lists.
+
+// My_answer
+function comp(array1, array2) {
+  if (Array.isArray(array1) && Array.isArray(array2)) {
+    return array2.map(n => Math.sqrt(n)).sort().toString() == array1.sort().toString();
+  } else {
+    return false;
+  }
+}
+
+// Best_answer
+function comp(array1, array2) {
+  if (array1 == null || array2 == null) return false;
+  array1.sort((a, b) => a - b); array2.sort((a, b) => a - b);
+  return array1.map(v => v * v).every((v, i) => v == array2[i]);
+}
