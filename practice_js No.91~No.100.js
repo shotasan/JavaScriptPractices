7626b789ef07f779112f139b08d7bb7f77f3c289
shotasan <wondermoai@gmail.com>
2019-10-08 11:54:40 +0900

    No.93

--- practice_js No.91~No.100.js
+++ practice_js No.91~No.100.js
@@ -47,3 +47,37 @@ function removeDuplicateWords(s) {
 // Set オブジェクトは値のコレクションです。挿入順に要素を反復することができます。
 // Set に重複する値は格納出来ません。Set 内の値はコレクション内で一意となります。
 const removeDuplicateWords = s => [...new Set(s.split(' '))].join(' ')
+
+
+// No.93
+// Complete the function caffeineBuzz, which takes a non - zero integer as it's one argument.
+// If the integer is divisible by 3, return the string "Java".
+// If the integer is divisible by 3 and divisible by 4, return the string "Coffee"
+// If the integer is one of the above and is even, add "Script" to the end of the string.
+// Otherwise, return the string "mocha_missing!"
+// caffeineBuzz(1)   => "mocha_missing!"
+// caffeineBuzz(3)   => "Java"
+// caffeineBuzz(6)   => "JavaScript"
+// caffeineBuzz(12)  => "CoffeeScript"
+
+// My_answer
+function caffeineBuzz(n) {
+  let result = [];
+  if (n % 3 === 0 && n % 4 === 0) {
+    result.push('Coffee');
+  } else if (n % 3 === 0) {
+    result.push('Java');
+  } else {
+    result.push('mocha_missing!');
+  }
+  if (n % 2 === 0 && result[0] !== 'mocha_missing!') result.push('Script');
+  return result.join('')
+}
+
+// Best_answer
+function caffeineBuzz(n) {
+  if (n % 12 === 0) return "CoffeeScript";
+  if (n % 6 === 0) return "JavaScript";
+  if (n % 3 === 0) return "Java";
+  return "mocha_missing!";
+}
