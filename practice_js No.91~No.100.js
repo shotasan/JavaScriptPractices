831165018924882ce9849edf591f3938b7dff64d
shotasan <wondermoai@gmail.com>
2019-10-17 11:06:54 +0900

    No.95

--- practice_js No.91~No.100.js
+++ practice_js No.91~No.100.js
@@ -122,3 +122,37 @@ DNAStrand.pairs = {
   C: 'G',
   G: 'C',
 }
+
+
+// No.95
+// #Get the averages of these numbers
+// Write a method, that gets an array of integer - numbers and return an array of the averages of each integer - number and his follower, if there is one.
+// Example:
+// Input: [1, 3, 5, 1, -10]
+// Output: [2, 4, 3, -4.5]
+// If the array has 0 or 1 values or is null or None, your method should return an empty array.
+// Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+// My_answer
+function averages(numbers) {
+  if (Array.isArray(numbers) && numbers.length) {
+    let result = [];
+    for (let i = 1; i < numbers.length; i++) {
+      result.push((numbers[i] + numbers[i - 1]) / 2);
+    }
+    return result;
+  } else {
+    return [];
+  }
+}
+
+// Best_answer
+function averages(numbers) {
+  var final = [];
+  if (numbers) {
+    for (var i = 0; i < numbers.length - 1; i++) {
+      final.push((numbers[i] + numbers[i + 1]) / 2);
+    }
+  }
+  return final;
+}
