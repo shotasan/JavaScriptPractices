6db04dbfb428800815886263036e8015577f89ec
shotasan <wondermoai@gmail.com>
2019-10-20 21:33:10 +0900

    No.98

--- practice_js No.91~No.100.js
+++ practice_js No.91~No.100.js
@@ -196,3 +196,25 @@ function vampire_test(a, b) {
 }
 
 function sortStr(v) { return v.split('').sort().join('') }
+
+
+// No.98
+// King Arthur and his knights are having a New Years party.Last year Lancelot was jealous of Arthur, because Arthur had a date and Lancelot did not, and they started a duel.
+// To prevent this from happening again, Arthur wants to make sure that there are at least as many women as men at this year's party. He gave you a list of integers of all the party goers.
+// Arthur needs you to return true if he needs to invite more women or false if he is all set.
+// Input / Output
+// [input] integer array L($a in PHP)
+// An array(guaranteed non - associative in PHP) representing the genders of the attendees, where - 1 represents women and 1 represents men.
+// 2 <= L.length <= 50
+// [output] a boolean value
+// true if Arthur need to invite more women, false otherwise.
+
+// My_answer
+function inviteMoreWomen(L) {
+  return L.reduce((sum, v) => sum + v) > 0;
+}
+
+// Best_answer
+function inviteMoreWomen(L) {
+  return L.reduce((a, b) => a + b) > 0;
+}
