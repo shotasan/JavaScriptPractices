f793a16a1b91564aa20455d26f47bd025c16063d
shotasan <wondermoai@gmail.com>
2019-12-08 23:30:35 +0900

    プロトタイプオブジェクトへの暗黙の参照

--- js_fundamental/prototype.js
+++ js_fundamental/prototype.js
@@ -5,8 +5,22 @@ var Member = function (firstName, lastName) {
 
 var mem = new Member("tanaka", "tarou");
 
+// 暗黙の参照によりインスタンス生成後にメソッドを追加できる
 Member.prototype.getName = function () {
   return this.firstName + '' + this.lastName;
 };
 
 console.log(mem.getName());
+
+
+
+// インスタンスにプロパティが存在する場合はプロタイプオブジェクトへの参照は行われない
+Member.prototype.sex = "Man";
+var mem1 = new Member();
+var mem2 = new Member();
+
+console.log(mem1.sex + mem2.sex);
+
+mem2.sex = "Woman";
+
+console.log(mem1.sex + mem2.sex);
