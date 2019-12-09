0c650592aa6c1666610546b20df0a32da891ed26
shotasan <wondermoai@gmail.com>
2019-12-09 23:29:58 +0900

    継承関係の動的な変更

--- js_fundamental/chain.js
+++ js_fundamental/chain.js
@@ -22,3 +22,23 @@ var dog = new Dog();
 dog.walk();
 dog.bark();
 
+
+
+// 動的な継承関係
+SuperAnimal = function () { };
+SuperAnimal.prototype = {
+  walk: function () {
+    console.log("ダダだだだだ");
+  }
+};
+
+var dog1 = new Dog();
+dog1.walk();
+
+// 継承関係を動的に切り替え
+Dog.prototype = new SuperAnimal();
+
+var dog2 = new Dog();
+dog2.walk();
+// インスタンス生成時の継承関係がインスタンスに固定される
+dog1.walk();
