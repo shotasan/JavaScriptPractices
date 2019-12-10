a80de5ea8d007afd9b7d3c75ef6baaf7c3cc0ca1
shotasan <wondermoai@gmail.com>
2019-12-11 02:16:47 +0900

    in演算子

--- js_fundamental/obj_type.js
+++ js_fundamental/obj_type.js
@@ -13,3 +13,9 @@ console.log(h.constructor === Hamster);
 // instanceof演算子はオブジェクトが特定のコンストラクターによって生成されたインスタンスであるかどうかを判定する
 console.log(h instanceof Animal);
 console.log(h instanceof Hamster);
+
+
+// メンバーの有無を判定するin演算子
+var obj = { hoge: function () { }, foo: function () { } };
+console.log("hoge" in obj);
+console.log("piyo" in obj);
