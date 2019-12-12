b61c2c4fe9337ec4c1bdc96fbe60e640801dbce7
shotasan <wondermoai@gmail.com>
2019-12-12 15:07:20 +0900

    get/set構文

--- js_fundamental/class.js
+++ js_fundamental/class.js
@@ -13,3 +13,39 @@ class Member {
 let m = new Member("Tarou", "Yamada");
 console.log(m.getName());
 
+
+// get/set構文によるプロパティの定義
+
+class Member {
+  constructor() {
+    let _firstName;
+    let _lastName;
+  }
+
+  get firstName() {
+    return this._firstName;
+  }
+
+  set firstName(value) {
+    this._firstName = value;
+  }
+
+  get lastName() {
+    return this._lastName;
+  }
+
+  set lastName(value) {
+    this._lastName = value;
+  }
+
+  getName() {
+    return this.lastName + this.firstName;
+  }
+}
+
+let m = new Member();
+m.firstName = "yamada";
+m.lastName = "tarou";
+console.log(m.firstName);
+console.log(m.lastName);
+console.log(m.getName());
