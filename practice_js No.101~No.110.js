2064396240128a81b3d1b5e374c3b1a5d49dd4ff
shotasan <45532225+shotasan@users.noreply.github.com>
2022-10-06 23:28:06 +0900

    No.106

--- practice_js No.101~No.110.js
+++ practice_js No.101~No.110.js
@@ -133,3 +133,35 @@ function toCamelCase(str){
   const regex = new RegExp('[-_](.)', 'ig');
   return str.replace(regex, (match, p1) => p1.toUpperCase());
 }
+
+// No.106
+// Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+// Examples
+// "4556364607935616" --> "############5616"
+//      "64607935616" -->      "#######5616"
+//                "1" -->                "1"
+//                 "" -->                 ""
+
+// // "What was the name of your first pet?"
+
+// "Skippy" --> "##ippy"
+
+// "Nananananananananananananananana Batman!"
+// -->
+// "####################################man!"
+
+function maskify(cc) {
+  return cc.slice(0, -4).replace(/./g, '#') + cc.slice(-4);
+}
+
+function maskify(cc) {
+  return cc.replace(/.(?=.{4})/g, "#");
+}
+
+// My Answer
+function maskify(cc) {
+  return cc.replace(/(.+)(.{4})$/g, (match, p1, p2) => '#'.repeat(p1.length) + p2);
+}
